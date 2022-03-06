f1699527e450f431cc46651b71f3de6683f1d741
medilies <medilies@gmail.com>
2022-03-06 15:51:10 +0100

    Hello react

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,14 @@
 require("./bootstrap");
+
+// resources/js/components/HelloReact.js
+
+import React from "react";
+import ReactDOM from "react-dom";
+
+export default function App() {
+    return <h1>Hello React!</h1>;
+}
+
+if (document.getElementById("app")) {
+    ReactDOM.render(<App />, document.getElementById("app"));
+}
