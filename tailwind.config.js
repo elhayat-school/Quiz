c6d13ca23e9b8cefa7c6535c8217f49bc7e522d7
medilies <medilies@gmail.com>
2022-03-06 15:50:37 +0100

    init

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    content: [
+        "./resources/**/*.blade.php",
+        "./resources/**/*.js",
+        "./resources/**/*.vue",
+        "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+};
