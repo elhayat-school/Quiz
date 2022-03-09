0df81d1c0314bc177d1a5f377a2206cf817e87c7
medilies <medilies@gmail.com>
2022-03-09 23:49:44 +0100

    react router init

--- resources/js/FrontEnd.jsx
+++ resources/js/FrontEnd.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 //
 import QuestionForm from "./components/QuestionForm";
 import LoginForm from "./components/LoginForm";
@@ -17,9 +18,12 @@ const FrontEnd = (props) => {
     return (
         <div className="bg-stone-300 flex-1">
             <h2 className="text-center font-bold"> {time} </h2>
-
-            <LoginForm />
-            <QuestionForm />
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/play" element={<QuestionForm />} />
+                    <Route path="/login" element={<LoginForm />} />
+                </Routes>
+            </BrowserRouter>
         </div>
     );
 };
