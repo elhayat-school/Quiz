369f6fd8380157cfc615f77d58928e075a8a48d4
medilies <medilies@gmail.com>
2022-03-13 11:33:37 +0100

    basic quiz.create form

---
+++ resources/views/quiz/create.blade.php
@@ -0,0 +1,33 @@
+<form action='{{ route('quiz.store') }}' method="POST">
+
+    @csrf
+
+    <input type="datetime-local" name="start_at" required>
+
+    <hr>
+    <hr>
+
+    @for ($i = 1; $i < 5; $i++)
+        <label for="questions[{{ $i }}][content]">
+            Question
+        </label>
+        <textarea name="questions[{{ $i }}][content]" cols="30" rows="10" required></textarea>
+
+
+        <hr>
+
+        @for ($j = 1; $j < 5; $j++)
+            <label for="questions[{{ $i }}][choices][{{ $j }}]">
+                Choix {{ $j }}
+            </label>
+            <input type="radio" name="questions[{{ $i }}][correct]" value="{{ $j }}" required>
+            <textarea name="questions[{{ $i }}][choices][{{ $j }}]" cols="30" rows="8"></textarea>
+        @endfor
+
+        <hr>
+        <hr>
+    @endfor
+
+    <button type="submit"> Ajouter </button>
+
+</form>
