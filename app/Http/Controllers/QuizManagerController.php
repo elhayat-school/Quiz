a698a5576ce92e4918c628ebbb58675f0ab7a50c
medilies <medilies@gmail.com>
2022-03-14 10:32:11 +0100

    FEAT: serving the actual first question

--- app/Http/Controllers/QuizManagerController.php
+++ app/Http/Controllers/QuizManagerController.php
@@ -7,33 +7,22 @@ use Illuminate\Http\Request;
 
 class QuizManagerController extends Controller
 {
+
+    public const TOO_EARLY = "TOO_EARLY";
+    public const PLAYING = "PLAYING";
+    public const TOO_LATE = "TOO_LATE";
+
     public function getQuestion()
     {
-        $quiz = Quiz::currentQuiz(); // Quiz already started
+        $quiz = Quiz::with('questions.choices')->currentQuiz();
 
         $response = $this->presetResponseForQuizTimeContext(strtotime($quiz->start_at), $quiz->duration);
 
-        /* ************************************************* */
-        //      SIMULATING GETTING A REAL QUESTION
-        /* ************************************************* */
-        if ($response['status'] === 'PLAYING') {
+        if ($response['status'] !== self::PLAYING)
+            return response()->json($response);
 
-            $lorem = '"لوريم ايبسوم دولار سيت أميت ,كونسيكتيتور أدايبا يسكينج أليايت,سيت دو أيوسمود تيمبورأنكايديديونتيوت لابوري ات دولار ماجنا أليكيوا . يوت انيم أدمينيم فينايم,كيواس نوستأكسير سيتاشن يللأمكو لابورأس نيسي يت أليكيوب أكس أيا كوممودو كونسيكيوات . ديواسأيوتي أريري دولار إن ريبريهينديرأيتفوليوبتاتي فيلايت أيسسي كايلليوم دولار أيو فيجايتنيولا باراياتيور. أيكسسيبتيور ساينت أوككايكات كيوبايداتات نون بروايدينت ,سيونت انكيولباكيو أوفيسيا ديسيريونتموليت انيم أيدي ايست لابوريوم."
-            "سيت يتبيرسبايكياتيس يوندي أومنيس أستي ناتيس أيررور سيت فوليبتاتيم أكيسأنتييومدولاريمكيو لايودانتيوم,توتام ريم أبيرأم,أيكيو أبسا كيواي أب أللوأنفينتوري فيرأتاتيس ايتكياسي أرشيتيكتو بيتاي فيتاي ديكاتا سيونت أكسبليكابو. نيمو أنيم أبسام فوليوباتاتيم كيوايفوليوبتاس سايت أسبيرناتشر أيوتأودايت أيوت فيوجايت, سيد كيواي كونسيكيونتشر ماجنايدولارس أيوس كيواي راتاشن فوليوبتاتيم سيكيواي نيسكايونت. نيكيو بوررو كيوايسكيومايست,كيوايدولوريم ايبسيوم كيوا دولار سايت أميت, كونسيكتيتيور,أديبايسكاي فيلايت, سيدكيواي نون نيومكيوام ايايوس موداي تيمبورا انكايديونت يوت لابوري أيتدولار ماجنامألايكيوام كيوايرات فوليوبتاتيم. يوت اينايم أد مينيما فينيام, كيواس نوستريوم أكسيركايتاشيميلامكوربوريس سيوسكايبيت لابورايوسام, نايساييوت ألايكيوايد أكس أيا كومودايكونسيكيواتشر؟ كيوايس أيوتيم فيل أيوم أيوري ريبريهينديرايت كيواي ان إيا فوليوبتاتيفيلايت ايسسي كيوم نايهايلموليستايا كونسيكيواتيو,فيلايليوم كيواي دولوريم أيوم فيوجايات كيوفوليوبتاس نيولا باراياتيور؟"';
-
-            $response['body'] = [ // APPENDING THE QUESTION
-                'question' => [
-                    'content' => mb_substr($lorem, rand(30, 50), rand(60, 80)),
-                    'choices' => [
-                        ['nb' => 1, 'content' => mb_substr($lorem, rand(150, 170), rand(180, 200))],
-                        ['nb' => 2, 'content' => mb_substr($lorem, rand(120, 140), rand(150, 170))],
-                        ['nb' => 3, 'content' => mb_substr($lorem, rand(200, 220), rand(230, 250))],
-                        ['nb' => 4, 'content' => mb_substr($lorem, rand(100, 120), rand(130, 150))],
-                    ]
-                ],
-            ];
-        }
-        /* ************************************************* */
+        // NOT AS SIMPLE AS THIS
+        $response['body']['question'] = $quiz->questions->first();
 
         return response()->json($response);
     }
@@ -49,14 +38,12 @@ class QuizManagerController extends Controller
         $time_diff = $start_at - date(time()); // ! Timezone
 
         if ($time_diff > 0)
-            return ['success' => false, 'status' => 'TOO_EARLY' , 'diff' => $time_diff]; // * append T0
+            return ['success' => false, 'status' => self::TOO_EARLY, 'body' => ['t0' => $start_at]]; // * append T0
 
         elseif ($time_diff >= -$duration && $time_diff <= 0)
-            return ['success' => true, 'status' => 'PLAYING' , 'diff' => $time_diff]; // * append a question
+            return ['success' => true, 'status' => self::PLAYING]; // * append a question
 
-        elseif ($time_diff < -180)
-            return ['success' => false, 'status' => 'TOO_LATE' , 'diff' => $time_diff]; // * append ...
+        elseif ($time_diff < -$duration)
+            return ['success' => false, 'status' => self::TOO_LATE, 'body' => ['t0' => $start_at]]; // * append ...
     }
-
-
 }
