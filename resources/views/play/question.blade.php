3b48c60106e588eaf596ab4d36606b47ffb030e3
abdimed <abdi.mohammed.dev@gmail.com>
2022-03-22 13:31:08 +0100

    Login-UI

--- resources/views/play/question.blade.php
+++ resources/views/play/question.blade.php
@@ -1,14 +1,5 @@
 <x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            Jouez
-        </h2>
-    </x-slot>
-
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 bg-white border-b border-gray-200">
+                <div class="p-6 ">
 
                     <form action="{{ route('anwswer.store') }}" method="POST">
                         @csrf
@@ -42,9 +33,7 @@
                     </form>
 
                 </div>
-            </div>
-        </div>
-    </div>
+
 
     <script src="{{ asset('js/countdown.js') }}"></script>
 
