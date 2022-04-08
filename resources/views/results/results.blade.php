467c35069ac701f76d3f6d6e418f0eef73caf4bf
medilies <medilies@gmail.com>
2022-04-08 17:50:13 +0100

    REFACTOR: rankings table component

--- resources/views/results/results.blade.php
+++ resources/views/results/results.blade.php
@@ -14,39 +14,8 @@
         <div>
 
             <span class="text-amber-400  font-semibold text-xl" dir="ltr"> : نتائج الأوائل في الجولة الأخيرة</span>
-            <table class="text-white text-lg overflow-scroll m-auto mt-5">
 
-                <tr>
-                    <th class="border p-2"> المرتبة </th>
-                    <th class="border p-2"> المشارك </th>
-                    <th class="border p-2"> عدد الإجابات الصحيحة </th>
-                    <th class="border p-2"> المدة الزمنية
-                    </th>
-                </tr>
-
-                <tbody>
-
-                    @foreach ($results as $rank => $result)
-                        @if (isset($results->limit) && $rank >= $results->limit)
-                            @if ($result->user_id !== auth()->user()->id)
-                                @php
-                                    continue;
-                                @endphp
-                            @elseif ($result->user_id === auth()->user()->id)
-                                <tr class="h-8"></tr>
-                            @endif
-                        @endif
-
-                        <tr class="{{ $result->user_id === auth()->user()->id ? 'bg-green-100 bg-opacity-20' : '' }}">
-                            <td class="border p-2 text-center">{{ $rank + 1 }}</td>
-                            <td class="border p-2 text-center">{{ $result->user->name }}</td>
-                            <td class="border p-2 text-center">{{ $result->count_correct_answers }} </td>
-                            <td class="border p-2 text-center">{{ $result->sum_elapsed_seconds }} ثانية</td>
-                        </tr>
-                    @endforeach
-                </tbody>
-
-            </table>
+            <x-tables.ranking :results="$results" />
 
         </div>
 
