6e31b9894fb20bb8596f51cbac09a1f743902cb0
medilies <medilies@gmail.com>
2022-04-10 12:05:57 +0100

    CONFIG: QUIZ_*

--- config/quiz.php
+++ config/quiz.php
@@ -2,8 +2,7 @@
 
 return [
     'QUIZ_ALLOW_DELAY' => env('QUIZ_ALLOW_DELAY', false),
-    'QUIZ_MAX_DELAY' => env('QUIZ_MAX_DELAY', 25),
-    'QUESTION_MAX_DURATION' => env('QUESTION_MAX_DURATION', 25),
+    'QUIZ_MAX_DELAY' => env('QUIZ_MAX_DELAY', 15),
     'QUESTION_DEFAULT_DURATION' => env('QUESTION_DEFAULT_DURATION', 20),
     'ESTABLISHMENTS' => ['Sabah', 'Gambetta', 'Maraval'],
 ];
