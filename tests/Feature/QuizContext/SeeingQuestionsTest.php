58e0d502257be8b338c1dc3a0451466a0634cece
medilies <medilies@gmail.com>
2022-04-18 15:05:43 +0100

    REFACTOR: test RecordAnswers helper

--- tests/Feature/QuizContext/SeeingQuestionsTest.php
+++ tests/Feature/QuizContext/SeeingQuestionsTest.php
@@ -2,12 +2,12 @@
 
 namespace Tests\Feature\QuizContext;
 
-use App\Models\Question;
 use App\Models\User;
 use App\Services\FullQuizSeed;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Support\Facades\Auth;
+use Tests\Helpers\RecordAnswers;
 use Tests\TestCase;
 
 class SeeingQuestionsTest extends TestCase
@@ -44,7 +44,7 @@ class SeeingQuestionsTest extends TestCase
 
         $quiz_example_1 = FullQuizSeed::seed($wait);
 
-        $this->recordAnswers($question_index);
+        RecordAnswers::exec($question_index);
 
         $this->seesQuestionAndItsChoices($quiz_example_1['questions'][$question_index]);
     }
@@ -55,37 +55,6 @@ class SeeingQuestionsTest extends TestCase
             ->assertSee($question['content']);
         foreach ($question['choices'] as $choice) {
             $response->assertSee($choice);
-        }
-    }
-
-    private function recordAnswers(int $last_answer_index = 0): void
-    {
-        // echo "================== $last_answer_index";
-
-        $questions = Question::all();
-
-        if ($last_answer_index > $questions->count())
-            throw new \Exception("Cannot record $last_answer_index answers for the quiz", 1);
-
-        for ($i = 0; $i <= $last_answer_index; $i++) {
-            if ($i < $last_answer_index) {
-
-                $ans = $questions[$i]->answers()->create([
-                    'user_id' => auth()->user()->id,
-                    'choice_number' => rand(1, 4),
-                    // Introduce multiplication
-                    'served_at' => date('Y-m-d H:i:s',  time() - config('quiz.QUESTION_DEFAULT_DURATION') - 2),
-                    'received_at' => date('Y-m-d H:i:s',  time() - 5),
-                ]);
-                // dump($ans->toArray());
-            } else {
-                $ans = $questions[$i]->answers()->create([
-                    'user_id' => auth()->user()->id,
-                    // Introduce multiplication
-                    'served_at' => date('Y-m-d H:i:s',  time() - 2),
-                ]);
-                // dump($ans->toArray());
-            }
         }
     }
 }
