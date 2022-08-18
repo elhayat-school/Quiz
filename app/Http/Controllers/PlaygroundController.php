23c8752f5ccf43b80e9a904acefd0628eca0dc3e
medilies <medilies@gmail.com>
2022-08-18 20:50:33 +0100

    replace ugly switch with match

--- app/Http/Controllers/PlaygroundController.php
+++ app/Http/Controllers/PlaygroundController.php
@@ -10,26 +10,13 @@ class PlaygroundController extends Controller
 {
     public function getQuizContext(QuizContextData $contextData): View|Factory
     {
-        switch ($contextData->context) {
-            case QuizContextData::NO_QUIZZES_AVAILABLE:
-                return view('playground.no_available_quizzes');
-
-            case QuizContextData::EARLY:
-                return view('playground.early')
-                    ->with('seconds_to_wait', $contextData->secondsToQuizStart);
-
-            case QuizContextData::ENDED:
-                return view('playground.ended');
-
-            case QuizContextData::LATE:
-                return view('playground.late');
-
-            case QuizContextData::FINISHED:
-                return view('playground.finished');
-
-            default:
-                return view('playground.question')
-                    ->with('question', $contextData->question);
-        }
+        return match ($contextData->context) {
+            QuizContextData::NO_QUIZZES_AVAILABLE => view('playground.no_available_quizzes'),
+            QuizContextData::EARLY => view('playground.early')->with('seconds_to_wait', $contextData->secondsToQuizStart),
+            QuizContextData::ENDED => view('playground.ended'),
+            QuizContextData::LATE => view('playground.late'),
+            QuizContextData::FINISHED => view('playground.finished'),
+            default => view('playground.question')->with('question', $contextData->question),
+        };
     }
 }
